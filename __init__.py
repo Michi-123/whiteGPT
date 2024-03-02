0cded7af3747eee7d861f0a48c888dec32807234
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-03-02 22:53:46 +0900

    modifeid init

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('./')
 
-from whiteGPT.source import whiteGPT
+from whiteGPT.source import GPT
 from whiteGPT.utils.data.gpt_dataset import PrepareData
 from whiteGPT.utils.data.gpt_dataset import DataLoader
 from whiteGPT.utils.data.gpt_dataset import JpTextDataset
