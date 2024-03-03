518a3921309fa75ef27f3e57879449c407dfe1a3
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-03-03 09:59:33 +0900

    modifeid functions

--- utils/functions/visualize_attention_weights.py
+++ utils/functions/visualize_attention_weights.py
@@ -1,3 +1,5 @@
+#@title def visualize_attention_weights(attention_weights, parsed_corpus):
+
 # !apt-get -y install fonts-ipafont-gothic
 # !pip install japanize-matplotlib
 
@@ -12,12 +14,18 @@ def visualize_attention_weights(attention_weights, parsed_corpus):
     num_heads, context_size, _ = attention_weights.shape
 
     words = parsed_corpus.split()[:context_size]
-    
+
     # グラフの作成
-    fig, axs = plt.subplots(num_heads, 1, figsize=(10, 6 * num_heads))
-    
+    if num_heads == 1:
+        fig, axs = plt.subplots(figsize=(10, 6))
+        axs = [axs]  # Encapsulate the single axis in a list to unify the handling below
+    else:
+        fig, axs = plt.subplots(num_heads, 1, figsize=(10, 6 * num_heads))
+
+
     # 各ヘッドごとに重みを可視化
     for i in range(num_heads):
+        print('i', i)
         # x軸、y軸のラベルに単語を設定
         axs[i].imshow(attention_weights[i], cmap='hot', interpolation='nearest')
         axs[i].set_title(f'Head {i+1} Attention')
@@ -28,18 +36,18 @@ def visualize_attention_weights(attention_weights, parsed_corpus):
         axs[i].set_xlabel('Input Sequence')
         axs[i].set_ylabel('Output Sequence')
         axs[i].set_aspect('auto')
-    
+
     plt.tight_layout()
     plt.show()
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     # 単語の配列
-    words = ['I', 'love', 'natural', 'language', 'processing']
+    words = 'I love natural language processing'
 
     # 例として、ランダムな自己注意の重みを生成
-    num_heads = 8
-    seq_length = len(words)
+    num_heads = 2
+    seq_length = len(words.split())
     attention_weights = torch.rand(num_heads, seq_length, seq_length)
 
     # 重みを可視化
