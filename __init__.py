75317879b6204aab64a2675647da7ac3431ff1e4
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-03-29 00:53:35 +0900

    fix init

--- __init__.py
+++ __init__.py
@@ -3,6 +3,10 @@ sys.path.append('./')
 
 from whiteGPT.source.GPT import GPT
 import whiteGPT.source.word2vec as word2vec
+from whiteGPT.source.word2vec import Vocab
+from whiteGPT.source.word2vec import TextDataset
+from whiteGPT.source.word2vec import CBOW
+
 from whiteGPT.source.GPT import create_attention_mask
 from whiteGPT.utils.data.gpt_dataset import PrepareData
 from whiteGPT.utils.data.gpt_dataset import DataLoader
