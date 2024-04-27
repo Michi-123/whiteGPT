b5b9f88414e88d7442bcdcb0d6fbd04b6a73e4f0
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-04-27 14:29:49 +0900

    fix init

--- __init__.py
+++ __init__.py
@@ -15,4 +15,4 @@ from whiteGPT.utils.data.gpt_dataset import PrepareData
 from whiteGPT.utils.data.gpt_dataset import ClassifierDataset
 from whiteGPT.utils.functions.evaluate import Evaluate
 from whiteGPT.utils.functions.evaluate import classifier_test
-from whiteGPT.utils.functions.visualize_attention_weights import from visualize_attention_weights
+from whiteGPT.utils.functions.visualize_attention_weights import visualize_attention_weights
