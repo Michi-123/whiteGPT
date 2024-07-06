53a4039691b316ba5c34f6fa0d04f40d11029331
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-07-07 00:13:49 +0900

    fix GPT1 norm

--- source/GPT.py
+++ source/GPT.py
@@ -245,10 +245,7 @@ class GPT(nn.Module):
         # Transformer ブロック
         for block in self.transformer_block:
             x, w = block(x, mask)
-        
-        # 正規化(GPT-2仕様)
-        x = self.norm(x)
-        
+      
         x = x.view(-1, self.context_size * self.d_model)
         
         # 線形変換
