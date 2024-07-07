07bd4143bc23e39dac833ab89fd9328209072a1f
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-07-07 10:51:17 +0900

    add classifier_test2

--- utils/functions/evaluate.py
+++ utils/functions/evaluate.py
@@ -203,6 +203,44 @@ def classifier_test(model, mask, classifier, vocab, dataloader,):
     print('OK: {}/{}'.format(count_ok, i+1))
 
 
+def classifier_test2(model, mask, classifier, vocab, dataloader,):
+    index2word = vocab.index2word
+    model.eval()
+    answers = {0:'否定', 1:'肯定', 2:'中立' }
+
+    count_ok = 0
+    past = None
+    
+    for i, batch in enumerate(dataloader):
+        # print(i)
+        source = batch['source']
+        target = batch['target']
+
+        # 事前学習モデルからlogitsを取得
+        x, past, _ = model(source, past, mask)
+
+        # 分類ヘッドの追加
+        logits = classifier(x)
+        
+        pred_index = torch.argmax(logits[0]).squeeze().item()
+
+        for idx in source[0]:
+            word = index2word[idx.item()]
+            if word == '<PAD>': break
+            print(word ,end="")
+
+        target_index = target[0].item()
+
+        if pred_index == target_index:
+            result = 'OK'
+            count_ok += 1
+        else:
+            result = 'NG'
+
+        print(' -->', answers[pred_index], answers[target_index], result)
+    
+    print('OK: {}/{}'.format(count_ok, i+1))
+
 
 
 if __name__ == "__main__": 
