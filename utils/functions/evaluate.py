dd93fbdf0f4a09ae25ae15256d5a243d7251b66b
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-07-18 06:40:05 +0900

    add fine tuned process

--- utils/functions/evaluate.py
+++ utils/functions/evaluate.py
@@ -85,7 +85,7 @@ class Evaluate:
 
             print(self.dataset.index2word[index] ,end="")
   
-    def generate2(self, corpus, model, mask=None, max_token_size=500):
+    def generate2(self, corpus, model, mask=None, max_token_size=500, topk=0):
         model.eval()
         model.cpu()
         pad = self.dataset.word2index['<PAD>']
@@ -102,7 +102,7 @@ class Evaluate:
                 mask = create_pad_mask(inputs)
 
             outputs ,_, _ = model(inputs, None, mask)
-            if 1:
+            if topk == 0:
                 index = torch.argmax(outputs).item()
             else:
                 k = 3
@@ -116,6 +116,61 @@ class Evaluate:
             source = source[1:]
 
             print(self.dataset.index2word[index] ,end="")
+
+
+    def generate_fine_tuned(self, corpus, model, mask=None, max_token_size=500, top_k=0, top_p=0, eos='<EOS>'):
+        model.eval()
+        model.cpu()
+        pad = self.dataset.word2index['<PAD>']
+
+        source = self.dataset.sequence2indices(corpus)
+        source = source[:self.context_size]
+        #indices = copy.copy(source)
+        source = source + [pad] * (self.context_size - len(source))
+
+        for i in range(max_token_size):
+            inputs = [source]
+            # print('inputs', inputs)
+            inputs = torch.LongTensor(inputs).cpu()
+            
+            if mask is not None:
+                mask = create_pad_mask(inputs)
+
+            outputs ,_, _ = model(inputs, None, mask)
+            if top_k == 0:
+                index = torch.argmax(outputs).item()
+            elif top_k > 0:
+
+                topk_values, topk_indices = torch.topk(outputs, top_k)
+
+                # topk_values をSoftmax で確率分布に変換
+                topk_values = torch.softmax(topk_values, dim=-1)
+
+                # print('topk_values',topk_values)
+                
+                # top-p で切り捨て
+                # Handle the case where no values are greater than top_p
+                valid_indices = topk_values > top_p
+                if valid_indices.any():
+                    topk_indices = topk_indices[valid_indices]
+
+                    # k個の最大値からランダムに1つをサンプリング
+                    topk_index = torch.randint(0, topk_indices.size(0), (1,)) # Change size(1) to size(0) to get the first dimension
+                    index = topk_indices[topk_index.item()].tolist()
+                else:
+                    # If no values are greater than top_p, take the most probable index (index 0)
+                    index = topk_indices[0, 0].item()  # Access the first element of the first dimension
+
+                
+            #indices.append(index)
+            source.append(index)
+            source = source[1:]
+
+            print(self.dataset.index2word[index] ,end="")
+
+            if self.dataset.index2word[index] == eos:
+                break
+
 
     def input_tokens(self, corpus):
         pad = self.dataset.word2index["<PAD>"]
