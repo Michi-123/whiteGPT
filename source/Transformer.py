fed2e2f1a22ceb36ee04895280556055c0551be8
Michi-123 <hairstyle2004@yahoo.co.jp>
2024-08-29 19:46:50 +0900

    Add Transformer

---
+++ source/Transformer.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+"""# ライブラリーのインポート"""
+
+#@title import
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+
+# @title ScaledDotProductAttention
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self, d_model, attn_dropout=0.1):
+        super().__init__()
+        self.sqrt_d_k = d_model ** 0.5 # sqrt(d_k)と同じ
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v, casual_mask=None, padding_mask=None):
+        score = torch.matmul(q, k.transpose(2, 3)) / self.sqrt_d_k
+        if casual_mask is not None:
+            # infは数値ではないのでプログラムによってはエラーを起こします。
+            score = score.masked_fill(casual_mask == 1, -1e9)
+
+        if padding_mask is not None:
+            score = score.masked_fill(padding_mask.unsqueeze(1).unsqueeze(2) == 1, -1e9)
+
+
+        attn_weights = F.softmax(score, dim=-1)
+        # 特定の単語に注意を払いすぎないようにdropoutを適用します
+        attn_weights = self.dropout(attn_weights)
+        attention_output = torch.matmul(attn_weights, v)
+
+        return attention_output, attn_weights
+
+
+#@title  Multi-Head Attention
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_head, d_model, dropout=0.1):
+        super().__init__()
+        self.n_head = n_head
+        self.d_model = d_model
+        self.fc_q = nn.Linear(d_model, d_model)
+        self.fc_k = nn.Linear(d_model, d_model)
+        self.fc_v = nn.Linear(d_model, d_model)
+        self.attention = ScaledDotProductAttention(d_model, dropout)
+        self.fc = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, q, k, v,  casual_mask=None, padding_mask=None):
+        N = q.size(0) # バッチサイズ
+        qS = q.size(1) # ウィンドウサイズ
+        kS = k.size(1)
+        vS = v.size(1)
+        H = self.n_head # マルチヘッドの数
+        D = self.d_model // self.n_head # 潜在空間の次元。（Cross-Attentionの場合、個別に定義します）
+
+        # 線形変換
+        q = self.fc_q(q)
+        k = self.fc_k(k)
+        v = self.fc_v(v)
+
+        # 展開
+        q = q.view(N, qS, H, D)
+        k = k.view(N, kS, H, D)
+        v = v.view(N, vS, H, D)
+
+        # 転置
+        q = q.transpose(1, 2)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
+
+        # Scaled dot-product attention
+        x, w = self.attention(q, k, v, casual_mask, padding_mask)
+
+        # 転置
+        x = x.transpose(1, 2)
+
+        # 結合
+        x = x.contiguous()
+
+        # 展開
+        x = x.view(N, qS, -1)
+
+        # 線形変換
+        x = self.fc(x)
+
+        # 正則化
+        x = self.dropout(x)
+
+        return x, w
+
+# @title Encoder
+class Encoder(nn.Module):
+    def __init__(self, src_vocab_size, max_seq_length, num_layers, d_model, n_head, dropout):
+        super(Encoder, self).__init__()
+        self.d_model = d_model
+        self.encoder_embedding = nn.Embedding(src_vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(max_seq_length, d_model)
+        self.layers = nn.ModuleList([EncoderLayer( d_model, n_head, dropout) for _ in range(num_layers)])
+
+    def forward(self, x, padding_mask=None):
+        x = self.encoder_embedding(x) + self.positional_encoding(x)
+        x = x * math.sqrt(self.d_model)
+
+        for layer in self.layers:
+            x, w = layer(x, padding_mask)
+
+        return x
+
+#@title EncoderLayer
+class EncoderLayer(nn.Module):
+    # GPT-2
+    def __init__(self, d_model, n_head, dropout=0.1):
+        super(EncoderLayer, self).__init__()
+
+        self.norm_1 = nn.LayerNorm(d_model)
+        self.norm_2 = nn.LayerNorm(d_model)
+        self.self_attn = MultiHeadAttention(n_head, d_model, dropout)
+        self.ff = FeedForward(d_model, dropout)
+        self.dropout = nn.Dropout(dropout)
+
+        # Initialize gamma (weight) with N(0, 0.02)
+        nn.init.normal_(self.norm_1.weight, mean=0, std=0.02)
+        nn.init.normal_(self.norm_2.weight, mean=0, std=0.02)
+
+    # GPT-2
+    def forward(self, x, padding_mask=None):
+        _x = x
+        x = self.norm_1(x)
+        x, w = self.self_attn(x, x, x, padding_mask=padding_mask)
+        x = x + _x
+
+        # Residual x
+        _x = x
+
+        # Feed Forward
+        x = self.norm_2(x)
+        x = self.ff(x)
+        x = x + _x
+
+        return x, w
+
+# @title Decoder
+class Decoder(nn.Module):
+    def __init__(self, vocab_size, context_size, n_block, d_model, n_head, dropout=0.1):
+        super(Decoder, self).__init__()
+
+        self.vocab_size = vocab_size
+        self.context_size = context_size
+        self.d_model = d_model
+        self.n_head = n_head
+        self.n_block = n_block
+
+        self.token_embedding = nn.Embedding(vocab_size, d_model)
+        self.positional_encoding = PositionalEncoding(context_size, d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.layers = nn.ModuleList([DecoderLayer(d_model, n_head, dropout) for _ in range(self.n_block)])
+        self.norm = nn.LayerNorm(d_model)
+        self.fc = nn.Linear(d_model, vocab_size)
+
+    def forward(self, tgt, memory, tgt_mask=None, tgt_padding_mask=None, memory_padding_mask=None):
+        x = self.token_embedding(tgt) + self.positional_encoding(tgt)
+        x = x * math.sqrt(self.d_model)
+        x = self.dropout(x)
+
+        for layer in self.layers:
+            x, _ = layer(x, memory, tgt_mask, tgt_padding_mask, memory_padding_mask)
+
+        x = self.norm(x)
+        logits = self.fc(x)
+        return logits
+
+
+#@title DecoderLayer
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, n_head, dropout=0.1):
+        super(DecoderLayer, self).__init__()
+
+        self.norm_1 = nn.LayerNorm(d_model)
+        self.norm_2 = nn.LayerNorm(d_model)
+        self.norm_3 = nn.LayerNorm(d_model)
+        self.self_attn = MultiHeadAttention(n_head, d_model, dropout)
+        self.cross_attn = MultiHeadAttention(n_head, d_model, dropout)
+        self.ff = FeedForward(d_model, dropout)
+        self.dropout = nn.Dropout(dropout)
+
+        # Initialize gamma (weight) with N(0, 0.02)
+        nn.init.normal_(self.norm_1.weight, mean=0, std=0.02)
+        nn.init.normal_(self.norm_2.weight, mean=0, std=0.02)
+        nn.init.normal_(self.norm_3.weight, mean=0, std=0.02)
+
+    # GPT-2
+    def forward(self, x, memory, casual_mask=None, tgt_padding_mask=None, memory_padding_mask=None):
+        # Residual x
+        _x = x
+        x = self.norm_1(x)
+        x, _ = self.self_attn(x, x, x, casual_mask, padding_mask=tgt_padding_mask)
+        x = x + _x
+
+        # Residual x
+        _x = x
+
+        x = self.norm_2(x)
+        x, _ = self.cross_attn(x, memory, memory, casual_mask=None, padding_mask=memory_padding_mask)
+        x = x + _x
+
+        # Residual x
+        _x = x
+
+        # Feed Forward
+        x = self.norm_3(x)
+        x = self.ff(x)
+        x = x + _x
+
+        return x, None
+
+
+# @title Transformer
+class Transformer(nn.Module):
+    def __init__(self, src_vocab_size, tgt_vocab_size, d_model, n_head, num_encoder_layers, num_decoder_layers, max_seq_length, dropout=0.1):
+        super(Transformer, self).__init__()
+        self.encoder = Encoder(src_vocab_size, max_seq_length, num_encoder_layers, d_model, n_head, dropout=dropout)
+        self.decoder = Decoder(tgt_vocab_size, max_seq_length, num_decoder_layers, d_model, n_head, dropout=dropout)
+        self.d_model = d_model
+        self.tgt_vocab_size = tgt_vocab_size
+        self.max_seq_length = max_seq_length
+
+    def forward(self, src, tgt, tgt_casual_mask=None, tgt_padding_mask=None, src_padding_mask=None):
+        memory = self.encoder(src, src_padding_mask)
+
+        input_tensor = make_input_tensor(src.size(0), src.size(1)) # [2,15]
+        input_tensor = input_tensor.to(src.device)
+        # 一度に一文を推論
+        output = self.decoder(input_tensor, memory, tgt_casual_mask, tgt_padding_mask, src_padding_mask)
+
+        return output
+
+
--- utils/functions/transformer_function.py
+++ utils/functions/transformer_function.py
@@ -1,21 +1,35 @@
-#@title functions
+import re
 import torch
+from torch.utils.data import DataLoader
 
-def create_mask(src, tgt):
-    src_seq_len = src.shape[1]
-    tgt_seq_len = tgt.shape[1]
+   
+#@title create_casual_mask
+def create_casual_mask(seq_length):
+    # 全ての要素が1の行列を作成
+    attention_mask = torch.ones((seq_length, seq_length))
+    # 対角線より上を0に変換
+    attention_mask = torch.triu(attention_mask, diagonal=1)
+    # True/Falseに変換
+    attention_mask = attention_mask == 1
 
-    tgt_mask = generate_square_subsequent_mask(tgt_seq_len).to(src.device)
-    src_padding_mask = (src == 0)
-    tgt_padding_mask = (tgt == 0)
+    return attention_mask
 
-    return tgt_mask, src_padding_mask, tgt_padding_mask
 
-def generate_square_subsequent_mask(sz):
+#@title create_padding_mask
+def create_padding_mask(sequence):
+    mask = (sequence == 0)
+    return mask
+    
+    
+def _generate_square_subsequent_mask(sz):
     mask = (torch.triu(torch.ones(sz, sz)) == 1)
     mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
     return mask
 
+#@title create_padding_mask
+def create_padding_mask(sequence):
+    mask = (sequence == 0)
+    return mask
 
 def _pad_collate(batch):
     src_list, tgt_list = [], []
@@ -27,3 +41,152 @@ def _pad_collate(batch):
     tgt_pad = nn.utils.rnn.pad_sequence(tgt_list, padding_value=0, batch_first=True)
 
     return src_pad, tgt_pad
+
+
+
+#@title make_input_tensor
+def make_input_tensor(batch_size, max_seq_length):
+    sos = 1
+    sequence_size = max_seq_length
+
+    # <sos>トークンを最初の要素とする1次元テンソルを生成
+    sos_tensor = torch.ones(batch_size, dtype=torch.long) * sos
+    # <pad>トークンを(batch_size, sequence_size-1)のテンソルとして生成
+    pad_tensor = torch.zeros(batch_size, sequence_size-1, dtype=torch.long)
+    # <sos>トークンと<pad>トークンを結合
+    input_tensor = torch.cat([sos_tensor.unsqueeze(1), pad_tensor], dim=1)
+
+    return input_tensor
+    
+    
+    
+    
+    
+import re
+import torch
+from torch.utils.data import DataLoader
+
+# @title test_from_dataset
+def test_from_dataset(model, dataset):
+    # デバイスの設定
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    PAD = 0
+    EOS = 2
+    model.eval()
+    model.to(device)
+
+    test_dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+    with torch.no_grad():
+        try:
+            for i, (src, tgt) in enumerate(test_dataloader):
+                src = src.to(device)
+                tgt = tgt.to(device)
+
+                context_size = src.size(1)
+
+                casual_tgt_mask = create_casual_mask(context_size).to(device)
+                tgt_padding_mask = create_padding_mask(tgt).to(device)
+                src_padding_mask = create_padding_mask(src).to(device)
+
+                tgt_input = None
+                output = model(src, tgt_input, casual_tgt_mask, tgt_padding_mask, src_padding_mask)
+
+                # 英語
+                print('原文:', end='')
+                src_indicies = src.squeeze(1).squeeze(0)
+                src_indicies = src_indicies.tolist()
+                for index in src_indicies:
+                    if index == PAD:
+                        break
+                    token = vocab_en.index2word[index]
+                    print(token, end=" ")
+                print()
+
+                # 正解データ
+                print('正解:', end='')
+                tgt_indices = tgt.squeeze(1).squeeze(0)
+                tgt_indices = tgt_indices.tolist()
+                for index in tgt_indices:
+                    if index == EOS:
+                        break
+                    token = vocab_ja.index2word[index]
+                    print(token, end="")
+                print()
+
+                # 生成された日本語
+                print('推論:', end='')
+                output = output.squeeze(1)[0]
+                output_indices = output.argmax(dim=1)
+                output_indices = output_indices.tolist()
+                for index in output_indices:
+                    if index == EOS:
+                        break
+                    token = vocab_ja.index2word[index]
+                    print(token, end="")
+
+                print('\n')
+
+                if (i+1) % 3 == 0:
+                    input('next')
+        except:
+            
+            print('Error.')
+
+
+#@title test_from_human_input
+def test_from_human_input(model, context_size):
+    PAD = 0
+    SOS = 1
+    EOS = 2
+    model.eval()
+    model.to(device)
+
+    sentence = input('英文')
+    sequence = normalizeString(sentence)
+    indices = []
+    for word in sequence.split():
+        try:
+            index = vocab_en.word2index[word]
+            indices.append(index)
+        except:
+            print('単語がありません')
+            return
+
+    indices = indices + [0] * context_size
+    indices = indices[:context_size]
+
+    input_tensor = torch.LongTensor(indices).unsqueeze(0)
+
+    tgt_casual_mask = create_casual_mask(context_size)
+
+    with torch.no_grad():
+        src = input_tensor.to(device)
+
+        tgt_casual_mask = create_casual_mask(context_size).to(device)
+        tgt_padding_mask = None
+        src_padding_mask = create_padding_mask(src).to(device)
+
+        tgt_input = None
+        output = model(src, tgt_input, tgt_casual_mask, tgt_padding_mask, src_padding_mask)
+
+        # 生成された日本語
+        print('推論:', end='')
+        output = output.squeeze(1)[0]
+        output_indices = output.argmax(dim=1)
+        output_indices = output_indices.tolist()
+        for index in output_indices:
+            if index == EOS:
+                break
+            token = vocab_ja.index2word[index]
+            print(token, end="")
+
+        print('\n')
+
+def normalizeString(sentence):
+    sentence = sentence.lower().strip()
+    sentence = re.sub(r"([.!?])", r" \1", sentence)
+    sentence = re.sub(r"[^a-zA-Z0-9.!?]+", r" ", sentence)
+    return sentence
+    
